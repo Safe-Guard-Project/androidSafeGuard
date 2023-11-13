9ad80484aafb052263e566c81548753a3c90fff2
dhiasaibi <dhiasaibi@yahoo.com>
2023-11-13 20:53:10 +0100

    Retrofit Instance done

---
+++ app/src/main/java/tn/esprit/safeguardapplication/repository/RetrofitInstance.kt
@@ -0,0 +1,25 @@
+package tn.esprit.safeguardapplication.repository
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import tn.esprit.safeguardapplication.Api.CatastropheApi
+
+object RetrofitInstance {
+    val api: CatastropheApi by lazy {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl("http://192.168.43.252:9090/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CatastropheApi::class.java)
+    }
+}
