a25c88b66cfe9162841f02afdc20e1a543492c2e
dhiasaibi <dhiasaibi@yahoo.com>
2023-11-13 19:09:54 +0100

    added implementation

--- app/src/main/java/tn/esprit/safeguardapplication/MainActivity.kt
+++ app/src/main/java/tn/esprit/safeguardapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package tn.esprit.safeguardapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 
-class MainActivity : AppCompatActivity() {
+const val TAG = "Main Activity"
+
+class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+
+
+}}
