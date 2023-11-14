e834d08263bbf30558da544f18cf162b1985579a
abir <abir.belkaroui@gmail.com>
2023-11-14 21:45:11 +0100

    interface d'introduction

--- app/src/main/java/tn/esprit/safeguardapplication/UI/Fragments/introFragment.kt
+++ app/src/main/java/tn/esprit/safeguardapplication/UI/Fragments/introFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import tn.esprit.safeguardapplication.R
+import tn.esprit.safeguardapplication.databinding.ActivityProgrammeBinding
+import tn.esprit.safeguardapplication.databinding.FragmentIntroBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,8 @@ class introFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var isFavFilled = false
+    private lateinit var binding: FragmentIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +38,32 @@ class introFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro, container, false)
+        binding = FragmentIntroBinding.inflate(inflater, container, false)
+        return binding.root
+
+        /*return inflater.inflate(R.layout.fragment_intro, container, false)*/
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Gérez le clic sur le bouton de cœur ici
+        binding.favBtnV.setOnClickListener {
+            // Changez l'image du cœur en fonction de l'état actuel
+            if (isFavFilled) {
+                // Si le cœur est rempli, changez à l'état vide
+                binding.favBtnV.setImageResource(R.drawable.ic_favoritevide)
+            } else {
+                // Si le cœur est vide, changez à l'état rempli
+                binding.favBtnV.setImageResource(R.drawable.ic_favorite)
+            }
+
+            // Inversez l'état actuel du cœur
+            isFavFilled = !isFavFilled
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment introFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             introFragment().apply {
