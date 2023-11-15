889b0de460b3f89f747cd92894342863130f46d3
Oussamabouriga <123689537+Oussamabouriga@users.noreply.github.com>
2023-11-15 18:46:58 +0100

    search done

--- app/src/main/java/tn/esprit/safeguardapplication/UI/Activities/MapActivity.kt
+++ app/src/main/java/tn/esprit/safeguardapplication/UI/Activities/MapActivity.kt
@@ -1,10 +1,21 @@
 package tn.esprit.safeguardapplication.UI.Activities
 
-import android.content.Intent
+
+
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.mapbox.android.gestures.MoveGestureDetector
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
@@ -16,11 +27,25 @@ import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
-import tn.esprit.safeguardapplication.MainActivity
+import com.mapbox.search.ResponseInfo
+import com.mapbox.search.SearchEngine
+import com.mapbox.search.SearchEngineSettings
+import com.mapbox.search.offline.OfflineResponseInfo
+import com.mapbox.search.offline.OfflineSearchEngine
+import com.mapbox.search.offline.OfflineSearchEngineSettings
+import com.mapbox.search.offline.OfflineSearchResult
+import com.mapbox.search.record.HistoryRecord
+import com.mapbox.search.result.SearchResult
+import com.mapbox.search.result.SearchSuggestion
+import com.mapbox.search.ui.adapter.engines.SearchEngineUiAdapter
+import com.mapbox.search.ui.view.CommonSearchViewConfiguration
+import com.mapbox.search.ui.view.DistanceUnitType
+import com.mapbox.search.ui.view.SearchResultsView
 import tn.esprit.safeguardapplication.R
 import tn.esprit.safeguardapplication.databinding.ActivityMapBinding
 import tn.esprit.safeguardapplication.util.LocationPermissionHelper
 import java.lang.ref.WeakReference
+
 
 class MapActivity : AppCompatActivity() {
 
@@ -46,53 +71,154 @@ class MapActivity : AppCompatActivity() {
 
         override fun onMoveEnd(detector: MoveGestureDetector) {}
     }
-    private lateinit var mapView: MapView
 
+    private lateinit var mapView: MapView
     private lateinit var binding: ActivityMapBinding
 
+
+
+    private lateinit var searchEngine: SearchEngine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        mapView = binding.mapview
-        binding.returnButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        mapView = binding.mapView
+
+
         mapView?.getMapboxMap()?.loadStyleUri(Style.TRAFFIC_DAY)
-        mapView = MapView(this)
-        setContentView(mapView)
         locationPermissionHelper = LocationPermissionHelper(WeakReference(this))
         locationPermissionHelper.checkPermissions {
             onMapReady()
         }
+        searchEngine = SearchEngine.createSearchEngineWithBuiltInDataProviders(
+            SearchEngineSettings(getString(R.string.mapbox_access_token))
+        )
+        val queryEditText = findViewById<EditText>(R.id.query_edit_text)
+        val searchResultsView = findViewById<SearchResultsView>(R.id.search_results_view)
+        searchResultsView.initialize(
+            SearchResultsView.Configuration(
+                commonConfiguration = CommonSearchViewConfiguration(DistanceUnitType.IMPERIAL)
+            )
+        )
+
+        val offlineSearchEngine = OfflineSearchEngine.create(
+            OfflineSearchEngineSettings(getString(R.string.mapbox_access_token))
+        )
+
+        val    searchEngineUiAdapter = SearchEngineUiAdapter(
+            view = searchResultsView,
+            searchEngine = searchEngine,
+            offlineSearchEngine = offlineSearchEngine,
+        )
+
+        searchEngineUiAdapter.addSearchListener(object : SearchEngineUiAdapter.SearchListener {
+
+
+
+            private fun showToast(message: String) {
+                Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onSuggestionsShown(suggestions: List<SearchSuggestion>, responseInfo: ResponseInfo) {
+                // not implemented
+            }
+
+            override fun onSearchResultsShown(
+                suggestion: SearchSuggestion,
+                results: List<SearchResult>,
+                responseInfo: ResponseInfo
+            ) {
+                // not implemented
+            }
+
+            override fun onOfflineSearchResultsShown(results: List<OfflineSearchResult>, responseInfo: OfflineResponseInfo) {
+                // not implemented
+            }
+
+            override fun onSuggestionSelected(searchSuggestion: SearchSuggestion): Boolean {
+                return false
+            }
+
+            override fun onSearchResultSelected(searchResult: SearchResult, responseInfo: ResponseInfo) {
+                showToast("SearchResult clicked: ${searchResult.name}")
+            }
+
+            override fun onOfflineSearchResultSelected(searchResult: OfflineSearchResult, responseInfo: OfflineResponseInfo) {
+                showToast("OfflineSearchResult clicked: ${searchResult.name}")
+            }
+
+            override fun onError(e: Exception) {
+                showToast("Error happened: $e")
+            }
+
+            override fun onHistoryItemClick(historyRecord: HistoryRecord) {
+                showToast("HistoryRecord clicked: ${historyRecord.name}")
+            }
+
+            override fun onPopulateQueryClick(suggestion: SearchSuggestion, responseInfo: ResponseInfo) {
+                queryEditText.setText(suggestion.name)
+            }
+
+            override fun onFeedbackItemClick(responseInfo: ResponseInfo) {
+                // not implemented
+            }
+        })
+        queryEditText.setOnClickListener {
+            // Trigger the search when the EditText is clicked
+            searchEngineUiAdapter.search(queryEditText.text.toString())
+        }
+        queryEditText.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, after: Int) {
+                if (s.length >= 3) {
+                    searchEngineUiAdapter.search(s.toString())
+                } else {
+                    // No need to manually set the adapter to null, let the SearchEngineUiAdapter handle it
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // not implemented
+            }
+
+            override fun afterTextChanged(e: Editable) {
+                // not implemented
+            }
+        })
+
+        // Check and request location permissions if needed
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                SearchActivity.PERMISSIONS_REQUEST_LOCATION
+            )
+        }
+
+    }
+
+
+    private companion object {
+        private const val PERMISSIONS_REQUEST_LOCATION = 0
+
+        fun Context.isPermissionGranted(permission: String): Boolean {
+            return ContextCompat.checkSelfPermission(
+                this, permission
+            ) == PackageManager.PERMISSION_GRANTED
+        }
     }
     private fun onMapReady() {
-        mapView.getMapboxMap().setCamera(
-            CameraOptions.Builder()
-                .zoom(14.0)
-                .build()
-        )
-        mapView.getMapboxMap().loadStyleUri(
-            Style.MAPBOX_STREETS
-        ) {
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) {
             initLocationComponent()
             setupGesturesListener()
 
-            // Use the safe call operator to handle potential null values
             val latitude = userLatitude
             val longitude = userLongitude
 
             if (latitude != null && longitude != null) {
                 mapView.getMapboxMap().setCamera(
                     CameraOptions.Builder()
-                        .center(
-                            com.mapbox.geojson.Point.fromLngLat(
-                                longitude,
-                                latitude
-                            )
-                        )
-                        .zoom(14.0)
+                        .center(com.mapbox.geojson.Point.fromLngLat(longitude, latitude))
+                        .zoom(12.0)
                         .build()
                 )
             }
@@ -106,7 +232,6 @@ class MapActivity : AppCompatActivity() {
     private var userLatitude: Double? = null
     private var userLongitude: Double? = null
 
-    // In your initLocationComponent() method, add the following code
     private fun initLocationComponent() {
         val locationComponentPlugin = mapView.location
         locationComponentPlugin.updateSettings {
@@ -135,28 +260,22 @@ class MapActivity : AppCompatActivity() {
             )
         }
 
-        // Add an OnIndicatorPositionChangedListener to get the user's location
         locationComponentPlugin.addOnIndicatorPositionChangedListener { point ->
             userLatitude = point.latitude()
             userLongitude = point.longitude()
             Log.d("User Location", "Latitude: $userLatitude, Longitude: $userLongitude")
-            // Use the user's location to make a call to your API
         }
 
         locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
     }
 
-
     private fun onCameraTrackingDismissed() {
-
         mapView.location
             .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
         mapView.location
             .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
         mapView.gestures.removeOnMoveListener(onMoveListener)
     }
-
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -166,7 +285,6 @@ class MapActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         locationPermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
-
 
     override fun onStart() {
         super.onStart()
