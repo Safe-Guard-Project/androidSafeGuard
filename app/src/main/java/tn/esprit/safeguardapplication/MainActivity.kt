27f2b39921cac75927eb28dba720c85bf97e887e
Ekbel <zrellliekbel@gmail.com$>
2023-11-19 14:26:12 +0100

    btn info

--- app/src/main/java/tn/esprit/safeguardapplication/MainActivity.kt
+++ app/src/main/java/tn/esprit/safeguardapplication/MainActivity.kt
@@ -4,18 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import tn.esprit.safeguardapplication.R.*
+import tn.esprit.safeguardapplication.UI.Fragments.InformationFragment
 import tn.esprit.safeguardapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = setContentView(layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        binding.info.setOnClickListener() {
-            val intent = Intent(this, InformationFragment::class.java)
-            startActivity(intent)
+            binding.info.setOnClickListener() {
+                val intent = Intent(this, InformationFragment::class.java)
+                startActivity(intent)
+            }
         }
     }
-}
