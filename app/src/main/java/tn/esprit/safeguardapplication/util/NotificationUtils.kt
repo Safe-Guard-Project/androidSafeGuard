8d14ee8a21a86d196d683e2e04897fd442572a22
dhiasaibi <dhiasaibi@yahoo.com>
2023-11-21 18:26:00 +0100

    notification design done

--- app/src/main/java/tn/esprit/safeguardapplication/util/NotificationUtils.kt
+++ app/src/main/java/tn/esprit/safeguardapplication/util/NotificationUtils.kt
@@ -1,17 +1,25 @@
 package tn.esprit.safeguardapplication.util
 import android.content.Context
 import android.app.NotificationManager
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import tn.esprit.safeguardapplication.R
 
 object NotificationUtils {
 
     fun showNotification(context: Context, name: String?, data: String?) {
+        val contentView = RemoteViews(
+            context.packageName,
+            R.layout.notification
+        )
+
+        contentView.setTextViewText(R.id.notifTitle, "You are in the radius of a catastrophe")
         val notificationBuilder = NotificationCompat.Builder(context, "channel_id")
             .setSmallIcon(R.drawable.earthquake)
             .setContentTitle(name)
             .setContentText(data)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContent(contentView)
 
         val notificationManager =
             context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
