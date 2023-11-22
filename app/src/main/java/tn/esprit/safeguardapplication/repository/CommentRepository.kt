11022105196c7a6d08bca63480bfee3df3ffae31
abir <abir.belkaroui@gmail.com>
2023-11-22 02:12:37 +0100

    get images Done

--- app/src/main/java/tn/esprit/safeguardapplication/repository/CommentRepository.kt
+++ app/src/main/java/tn/esprit/safeguardapplication/repository/CommentRepository.kt
@@ -1,8 +1,4 @@
 package tn.esprit.safeguardapplication.repository
 
-import retrofit2.Response
-import tn.esprit.safeguardapplication.models.Commentaire
-
-interface CommentRepository {
-    fun addComment(commentaire: Commentaire): Response<Commentaire>
+class CommentRepository {
 }
