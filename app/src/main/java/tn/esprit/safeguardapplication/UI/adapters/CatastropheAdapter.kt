b5f8aacfa80ca7a4689f2ad1ae3068909bc4fb63
dhiasaibi <dhiasaibi@yahoo.com>
2023-11-22 15:25:42 +0100

    search done

--- app/src/main/java/tn/esprit/safeguardapplication/UI/adapters/CatastropheAdapter.kt
+++ app/src/main/java/tn/esprit/safeguardapplication/UI/adapters/CatastropheAdapter.kt
@@ -2,22 +2,30 @@ package tn.esprit.safeguardapplication.UI.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import tn.esprit.safeguardapplication.databinding.ItemCatastropheBinding
 import tn.esprit.safeguardapplication.models.Catastrophe
 
-class CatastropheAdapter: RecyclerView.Adapter<CatastropheAdapter.CatastropheViewHolder>() {
-    inner class CatastropheViewHolder(val binding: ItemCatastropheBinding): RecyclerView.ViewHolder(binding.root)
+class CatastropheAdapter : RecyclerView.Adapter<CatastropheAdapter.CatastropheViewHolder>(),
+    Filterable {
 
-    private val diffCallback = object : DiffUtil.ItemCallback<Catastrophe>(){
+    private var originalList: List<Catastrophe> = emptyList()
+
+    inner class CatastropheViewHolder(val binding: ItemCatastropheBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Catastrophe>() {
         override fun areContentsTheSame(oldItem: Catastrophe, newItem: Catastrophe): Boolean {
             return oldItem._id == newItem._id
         }
 
         override fun areItemsTheSame(oldItem: Catastrophe, newItem: Catastrophe): Boolean {
-            return oldItem==newItem
+            return oldItem == newItem
         }
     }
 
@@ -25,16 +33,21 @@ class CatastropheAdapter: RecyclerView.Adapter<CatastropheAdapter.CatastropheVie
 
     var catastrophes: List<Catastrophe>
         get() = differ.currentList
-        set(value) { differ.submitList(value)}
+        set(value) {
+            originalList = value
+            differ.submitList(value)
+        }
 
-    override fun getItemCount()= catastrophes.size
+    override fun getItemCount() = catastrophes.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatastropheViewHolder {
-        return CatastropheViewHolder(ItemCatastropheBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        ))
+        return CatastropheViewHolder(
+            ItemCatastropheBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: CatastropheViewHolder, position: Int) {
@@ -43,6 +56,35 @@ class CatastropheAdapter: RecyclerView.Adapter<CatastropheAdapter.CatastropheVie
             tvType.text = "Type: ${catastrophe.type}"
             tvPlace.text = "Description: ${catastrophe.description}"
             tvMag.text = "Magnitude: ${catastrophe.magnitude.toString()} Richter "
+        }
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filteredList = mutableListOf<Catastrophe>()
+
+                if (constraint.isNullOrBlank()) {
+                    filteredList.addAll(originalList)
+                } else {
+                    val filterPattern = constraint.toString().toLowerCase().trim()
+
+                    for (catastrophe in originalList) {
+                        if (catastrophe.description.lowercase().contains(filterPattern)) {
+                            filteredList.add(catastrophe)
+                        }
+                    }
+                }
+
+                val results = FilterResults()
+                results.values = filteredList
+                return results
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                differ.submitList(results?.values as List<Catastrophe>)
+            }
         }
     }
 }
